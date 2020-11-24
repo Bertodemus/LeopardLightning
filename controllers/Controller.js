8291e3d5364d576411f05ba4e38776f37539c650
David Stinnett <davidstinnett@icloud.com>
2020-11-24 11:14:40 -0500

    started on controller and handlebars

---
+++ controllers/Controller.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+
+const models = require("./models.js")
+//Not 100% sure where to go from here, but it's a small start //DS 11/24
---
+++ controllers/models.js
@@ -0,0 +1,10 @@
+const comment = require("../models").Comment;
+const event = require("../models").Event;
+const index = require("../models").Index;
+const user = require("../models").User;
+
+module.exports = {
+    
+}
+
+//Not 100% where to go from here, but my goal is to import the models here and then import this file into the controller, to keep everything somewhat modular and logical. //DS 11/24
