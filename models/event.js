5a6c11365fcfda95d5d2c7f33bb33aae63e65a42
David Stinnett <davidstinnett@icloud.com>
2020-11-25 11:04:00 -0600

    formatting

--- models/event.js
+++ models/event.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = function(sequelize, DataTypes) {
   const Event = sequelize.define(
     "Event",
@@ -71,74 +70,3 @@ module.exports = function(sequelize, DataTypes) {
 
   return Event;
 };
-=======
-module.exports = function(sequelize, DataTypes){
-    const Event = sequelize.define("Event", {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        team_one_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        team_one_win: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        team_one_loss: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        team_one_genre: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        team_one_votes: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0
-        },
-        team_two_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        team_two_win: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        team_two_loss: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        team_two_genre: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        team_two_votes: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0
-        },
-    },
-    {
-        freezeTableName: true
-    });
-
-    Event.associate = (models) => {
-        Event.belongsToMany(models.Comment, {
-            through: "event_comment",
-            as: "comments",
-            foreignKey: "event_id",
-          });
-    };
-
-    return Event;
-};
->>>>>>> 4befe98f529aea6a0cec3f21891df57609b8d7d3
