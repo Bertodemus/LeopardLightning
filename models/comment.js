b4817d9e75f633edd27ed19955052b29f8042990
Kilroy <jordans1@vt.edu>
2020-11-28 18:09:42 -0500

    Fixed package issues

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = function(sequelize, DataTypes) {
   const Comment = sequelize.define(
     "Comment",
@@ -31,34 +30,3 @@ module.exports = function(sequelize, DataTypes) {
 
   return Comment;
 };
-=======
-module.exports = function(sequelize, DataTypes){
-    const Comment = sequelize.define("Comment", {
-        text: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        }
-    },
-    {
-        freezeTableName: true
-    });
-
-    Comment.associate = (models) => {
-        Comment.belongsToMany(models.Event, {
-            through: "event_comment",
-            as: "events",
-            foreignKey: "comment_id",
-          });
-    };
-
-    Comment.associate = (models) => {
-        Comment.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Comment;
-};
->>>>>>> 4befe98f529aea6a0cec3f21891df57609b8d7d3
