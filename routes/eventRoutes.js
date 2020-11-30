3c3d2a562918442c761672560769d22ebbd4d109
David Stinnett <davidstinnett@icloud.com>
2020-11-29 20:24:37 -0500

    controller and api stuff

---
+++ routes/eventRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const eventController = require("../controllers/Controller");
+const commentController = require("../controllers/commentController";
+const userController = require("../controllers/userController")
+
+router.get("/event", eventController.all);
+router.get("/event/:id", eventController.byId);
+router.post("/event", eventController.create);
+router.delete("/event/:id", eventController.remove);
+
+module.exports = router;
